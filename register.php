b37f874bc959a696107cbe37bb0b0ad1ed701f41
Babar Khan <babarkhan138@gmail.com>
2019-12-24 03:47:05 +0500

    create registration form

--- register.php
+++ register.php
@@ -21,7 +21,40 @@
                 <input type="password" id="loginPassword" name="loginPassword" required>
             </p>
 
-            <button type="submit">Login</button>
+            <button type="submit" name="loginButton">Login</button>
+        </form>
+        <form action="register.php" id="registerForm" method="POST">
+            <h3>Login to your account</h3>
+            <p>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="e.g. johnSmilga" required>
+            </p>
+            <p>
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="e.g. John" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" placeholder="e.g. Smilga" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="e.g. smilga@gmail.com" required>
+            </p>
+            <p>
+                <label for="email2">Confirm Email</label>
+                <input type="email" id="email2" name="email2" placeholder="e.g. smilga@gmail.com" required>
+            </p>
+            <p>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </p>
+            <p>
+                <label for="password2">Confirm Password</label>
+                <input type="password" id="password2" name="password2" required>
+            </p>
+
+            <button type="submit" name="registerButton">Sign Up</button>
         </form>
     </div>
 </body>
