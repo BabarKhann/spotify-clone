4e62f1d0021e5ccf8b06072552e91527dc5985eb
Babar Khan <babarkhan138@gmail.com>
2019-12-25 02:09:59 +0500

    Create sanitize functions for registration form fields

--- register.php
+++ register.php
@@ -1,3 +1,51 @@
+<?php
+
+function sanitizeFormUserName($value = null)
+{
+    $value = strip_tags($value);
+    $value = str_replace(" ", "", $value);
+    $value = trim($value);
+
+    return $value;
+}
+
+function sanitizeFormString($value = null)
+{
+    $value = strip_tags($value);
+    $value = str_replace(" ", "", $value);
+    $value = trim($value);
+    $value = ucfirst(strtolower($value));
+
+    return $value;
+}
+
+function sanitizeFormPassword($value = null)
+{
+    $value = strip_tags($value);
+
+    return $value;
+}
+
+// Login
+if (isset($_POST['loginButton'])) :
+    echo 'login button pressed';
+endif;
+
+// Register
+if (isset($_POST['registerButton'])) :
+    $username = sanitizeFormUserName($_POST['username']);
+    $firstName = sanitizeFormString($_POST['firstName']);
+    $lastName = sanitizeFormString($_POST['lastName']);
+    $firstName = sanitizeFormString($_POST['lastName']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+
+endif;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
