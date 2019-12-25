167c519715be298ca633279c9b5a1a2603cb9a9a
Babar Khan <babarkhan138@gmail.com>
2019-12-25 23:59:56 +0500

    validate the first name, last name and emails

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -27,14 +27,29 @@ class Account
 
     private function validateFirstName($value)
     {
+        if (strlen($value) < 2 || strlen($value > 25)) :
+            array_push($this->errors, "Your first name must be between 2 and 25 characters");
+            return;
+        endif;
     }
 
     private function validateLastName($value)
     {
+        if (strlen($value) < 2 || strlen($value > 25)) :
+            array_push($this->errors, "Your last name must be between 2 and 25 characters");
+            return;
+        endif;
     }
 
     private function validateEmails($value1, $value2)
     {
+        if ($value1 != $value2) :
+            array_push($this->errors, "Your emails don't match");
+        endif;
+
+        if (!filter_var($value1, FILTER_VALIDATE_EMAIL)) :
+            array_push($this->errors, "Email is invalid");
+        endif;
     }
 
     private function validatePasswords($value1, $value2)
