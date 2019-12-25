1e31a11fd5c13e55c00fa919e95b416cf51e3466
Babar Khan <babarkhan138@gmail.com>
2019-12-26 00:07:37 +0500

    validate the passwords

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -43,16 +43,31 @@ class Account
 
     private function validateEmails($value1, $value2)
     {
-        if ($value1 != $value2) :
+        if ($value1 !== $value2) :
             array_push($this->errors, "Your emails don't match");
         endif;
 
         if (!filter_var($value1, FILTER_VALIDATE_EMAIL)) :
             array_push($this->errors, "Email is invalid");
+            return;
         endif;
     }
 
     private function validatePasswords($value1, $value2)
     {
+        if ($value1 !== $value2) :
+            array_push($this->errors, "Your passwords don't match");
+            return;
+        endif;
+
+        if (preg_match('/[^A-Za-z0-9]/', $value1)) :
+            array_push($this->errors, "Your password can only contains numbers and letters");
+            return;
+        endif;
+
+        if (strlen($value1) < 5 || strlen($value1 > 20)) :
+            array_push($this->errors, "Your password must be between 5 and 30 characters");
+            return;
+        endif;
     }
 }
